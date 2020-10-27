86176a66d241b02b6673658146f7dc7c7a40fd3c
xujian <1227971544@qq.com>
2020-10-27 09:41:11 +0800

    完成output学习

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,18 @@ module.exports = {
   context: path.resolve(__dirname, 'src'),
   output: {
     // 输出文件都放到 dist 目录下
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, 'dist_[hash:8]'),
+    // 设置文件名
+    filename:'[name]_[chunkhash:8].js',
+    // 配置cdn公共路径
+    publicPath: 'https://cdn.example.com/assets/',
+    // 用于配置这个异步插入的script标签的 crossorigin 值
+    crossOriginLoading:'use-credentials',
+    // 编写的库将通过 var 被赋值给通过 library 指定名称的变量
+    library: 'LibraryName',
+    // 配置以何种方式导出库
+    libraryTarget: 'var' | 'commonjs' | 'commonjs2' | 'this' | 'window' | 'global',
+    // output.libraryExport 配置要导出的模块中哪些子模块需要被导出。 它只有在 output.libraryTarget 被设置成 commonjs 或者 commonjs2 时使用才有意义。
+    libraryExport:'show'
   }
 };
